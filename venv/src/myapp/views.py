4de5c0f1aed6a87845faaed459e1f67b983e1d7c
Dominik Tomczyk <tmzk95@gmail.com>
2017-06-01 18:24:41 +0200

    initial commit

---
+++ venv/src/myapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, render_to_response
+
+def index(request):
+    return render_to_response('index.html')
+def test(request):
+    return render_to_response('test.html')
