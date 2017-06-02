15638e4b895a5d68768119dacf60b3d3fb760f48
Tomek <ttomasz.rodzik@gmail.com>
2017-06-02 20:39:30 +0200

    Dodanie bazy danych restauracji i recenzji i dostepu do nich z poziomu admina

--- venv/src/myapp/admin.py
+++ venv/src/myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Recenzja,Restauracja
 
 # Register your models here.
+admin.site.register(Restauracja)
+admin.site.register(Recenzja)
--- venv/src/myapp/models.py
+++ venv/src/myapp/models.py
@@ -2,3 +2,18 @@ from django.db import models
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 
+class Restauracja(models.Model):
+    nazwa = models.CharField(max_length=200)
+    zdjecie= models.CharField(max_length=300)
+    opis = models.CharField(max_length=200)
+    ocena = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.nazwa
+
+
+class Recenzja(models.Model):
+    opis= models.CharField(max_length=1500)
+    ocena= models.FloatField
+    restauracja = models.ForeignKey(Restauracja, on_delete=models.CASCADE)
+    uzytkownik = models.ForeignKey(User, on_delete=models.CASCADE)
